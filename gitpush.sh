97c5eefbd1ad872962076c404b34a9a95e40dc5c
metou3 <ilovebass879@gmail.com>
2021-01-07 14:58:33 +0100

    update data

--- gitpush.sh
+++ gitpush.sh
@@ -1,3 +1,3 @@
 git add .
 git commit -m "update data"
-git push https://metou3:gitme123^^@github.com/metou3/n3ws_data.git
+git push -u origin master https://metou3:gitme123^^@github.com/metou3/n3ws_data.git
